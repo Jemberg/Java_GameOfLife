6f03d79d96344a93e8f15aaebfb2cc5af6c5ac94
s9_freibe_k <s9_freibe_k@venta.lv>
2021-06-12 12:50:11 +0300

    removed Cell class, and replaced it with a 2d boolean array in the Grid class

--- src/main/java/com/example/model/Grid.java
+++ src/main/java/com/example/model/Grid.java
@@ -1,5 +1,7 @@
 package com.example.model;
 
+
+
 import java.util.ArrayList;
 import java.util.Random;
 
@@ -7,7 +9,7 @@ public class Grid {
     private int iteration;
     private int rows;
     private int columns;
-    private Cell[][] cells;
+    private boolean[][] cells;
 
     public Grid(int rows, int columns){
         if(rows<=0 || columns<=0){
@@ -16,24 +18,15 @@ public class Grid {
         iteration = 0;
         this.rows = rows;
         this.columns = columns;
-        cells = generateCells();
+        cells = new boolean[rows][columns];
     }
 
-    private Cell[][] generateCells(){
-        Cell[][] cells = new Cell[rows][columns];       //i dont check if rows or columns are correct because the only function that is gonna call this is the constructor which already has a check
-        for(int i = 0; i<rows; i++){
-            for(int j = 0; j<rows; j++){
-                cells[i][j] = new Cell();
-            }
-        }
-        return cells;
-    }
 
     public int getIteration(){
         return iteration;
     }
 
-    public Cell getCell(int row, int column){
+    public Boolean getCellVal(int row, int column){
         return cells[row][column];
     }
 
@@ -45,15 +38,16 @@ public class Grid {
         return columns;
     }
 
-    public Cell[][] getCells() {
+    public boolean[][] getCells() {
         return cells;
     }
 
     public void toggleCellStatus(int row, int column){
-        if(rows<=0 || columns<=0){
+        if(row<=0 || column<=0){
             throw new RuntimeException("neither rows nor columns can be negative numbers");
-        }
-        cells[row][column].toggleCellStatus();
+        } else if(row>=rows || column>=columns){
+            throw new RuntimeException("index out of range");
+        } else cells[row][column]= !cells[row][column];
     }
 
     public void randomGeneration(Random random) {           ////idk if this works like that
@@ -62,7 +56,7 @@ public class Grid {
         }
         for (int i = 0; i < rows; i++) {
             for (int j = 0; j < getColumns(); j++) {
-                getCell(i, j).setAlive(random.nextBoolean());
+                cells[i][j] = random.nextBoolean();
             }
         }
     }
@@ -70,16 +64,16 @@ public class Grid {
     public void setAllDead(){
         for (int i = 0; i < rows; i++) {
             for (int j = 0; j < getColumns(); j++) {
-                getCell(i, j).setAlive(false);
+                cells[i][j] = false;
             }
         }
     }
 
-    public ArrayList<Cell> getNeighbourCells(int row, int column){
+    public ArrayList<Boolean> getNeighbourCells(int row, int column){
         if(column>columns-1||column<0||row>rows-1||row<0){
             throw new RuntimeException("index out of range");
         }
-        ArrayList<Cell> neighbours = new ArrayList<>();
+        ArrayList<Boolean> neighbours = new ArrayList<>();
         if(row+1<=rows-1){
             neighbours.add(cells[row+1][column]);
             if(column+1<=columns-1) neighbours.add(cells[row+1][column+1]);
@@ -102,9 +96,9 @@ public class Grid {
 
     public int aliveNeighbours(int row, int column){
         int aliveCounter = 0;
-        ArrayList<Cell> neighbours = getNeighbourCells(row,column);
-        for(Cell cell: neighbours){
-            if(cell.isAlive()){
+        ArrayList<Boolean> neighbours = getNeighbourCells(row,column);
+        for(Boolean cell: neighbours){
+            if(cell){
                 aliveCounter++;
             }
         }
@@ -115,8 +109,12 @@ public class Grid {
     public String toString(){
         String returnStr = "";
         for(int i = 0; i<=rows-1; i++){
-            for(Cell cell: cells[i]){
-                returnStr+=cell + " ";
+            for(boolean cell: cells[i]){
+                if(cell){
+                    returnStr+=1 + " ";
+                } else{
+                    returnStr+=0 + " ";
+                }
             }
             returnStr+="\n";
         }
@@ -129,15 +127,27 @@ public class Grid {
         for (int i = 0; i < rows; i++) {
             for (int j = 0; j < getColumns(); j++) {
                 int aliveNeighbours = aliveNeighbours(i,j);
-                if(cells[i][j].isAlive() && aliveNeighbours == 2 || aliveNeighbours == 3) newCellStates[i][j] = true;
+                if(cells[i][j] && aliveNeighbours == 2 || aliveNeighbours == 3) newCellStates[i][j] = true;
             }
         }
         //replaces current cell values with the ones we generated in the first step
         for (int i = 0; i < rows; i++) {
-            for (int j = 0; j < getColumns(); j++) {
-                cells[i][j].setAlive(newCellStates[i][j]);
-            }
+            if (getColumns() >= 0) System.arraycopy(newCellStates[i], 0, cells[i], 0, getColumns()); //TODO check how this works
         }
         iteration++;
+
+
     }
+
+//    public void saveAsJson(String filePath){
+//        JSONObject json = new JSONObject();
+//        json.put("grid_rows", rows);
+//        json.put("grid_columns", columns);
+//        json.put("iteration", iteration);
+//        json.put()
+//    }
+
+
+
+
 }
